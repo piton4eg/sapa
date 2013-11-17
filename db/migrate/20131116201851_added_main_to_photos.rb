ea31493f4bc1dbf7c8d9117baf88af3215065f92
Markov Alexey <piton4eg@mail.ru>
2013-11-17 19:34:56 +0400

    Fix errors

--- db/migrate/20131116201851_added_main_to_photos.rb
+++ db/migrate/20131116201851_added_main_to_photos.rb
@@ -1,8 +1,8 @@
 class AddedMainToPhotos < ActiveRecord::Migration
   def change
     change_table :photos do |t|
-      t.column :main_photo, :booleand, default: false
-      t.column :portfolio, :booleand, default: false
+      t.column :main_photo, :boolean, default: false
+      t.column :portfolio, :boolean, default: false
     end
   end
 end
