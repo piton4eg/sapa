e6e9f388d9cf4d2c7f30b491b898acc004098ad9
Markov Alexey <piton4eg@mail.ru>
2013-12-03 00:46:48 +0400

    Added font and fixed main page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,14 @@
 module ApplicationHelper
   def full_title(page_title)
-    base_title = "SAPA - Photo by Olga Markova"
-    page_title.empty? ? base_title : "#{base_title} | #{page_title}"
+    base_title = "Markova Olga"
+    page_title.empty? ? base_title : "#{page_title} - #{base_title}"
   end
 
   def current_link_to(label, path)
     content_tag('li', link_to(label, path), class: (current_page?(path) ? "active" : nil))
+  end
+
+  def logged_in?
+    session[:user_id].present?
   end
 end
