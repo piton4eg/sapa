6ca77726503920771aa892dd8659566186cb2e0c
Markov Alexey <piton4eg@mail.ru>
2014-11-22 16:13:20 +0300

    Add some css for signup page and flash messages

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ActiveRecord::Base
   authenticates_with_sorcery!
 
-  validates :password, length: { minimum: 3 }, confirmation: true, presence: true
+  validates :password, presence: true, length: { minimum: 3 }, confirmation: true
   validates :password_confirmation, presence: true
   validates :email, presence: true, uniqueness: true, email: true
 
